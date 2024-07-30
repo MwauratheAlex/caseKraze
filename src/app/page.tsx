8a0e63a8b5a549799e45fe0b7b99261a14660331
Mbugua <mwaurathealex@gmail.com>
2024-07-30 16:36:13 +0300

    upload page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="bg-slate-50">
-      <section>
+      <section className="overflow-x-hidden">
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
