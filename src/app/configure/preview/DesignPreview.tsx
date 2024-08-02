5633702ae9a95555f8dd64825350dee992103a63
Mbugua <mwaurathealex@gmail.com>
2024-08-02 13:35:03 +0300

    design preview

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -26,147 +26,142 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
 
   const [isLoginModalOpen, setLoginModalOpen] = useState<boolean>(false);
 
-  const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
-    const [showConfetti, setShowConfetti] = useState(false);
-    useEffect(() => setShowConfetti(true), []);
 
-    const { color, croppedImgUrl, model, finish, material } = configuration;
 
-    const tw = COLORS.find((supportedColor) => supportedColor.value === color)?.tw;
+  const tw = COLORS.find((supportedColor) => supportedColor.value === color)?.tw;
 
-    const { label: modelLabel } = MODELS.options.find(({ value }) => value === model)!;
+  const { label: modelLabel } = MODELS.options.find(({ value }) => value === model)!;
 
-    let totalPrice = BASE_PRICE;
-    if (material === "polycarbonate") totalPrice += PRODUCT_PRICES.material.polycarbonate;
-    if (finish === "textured") totalPrice += PRODUCT_PRICES.finish.textured;
+  let totalPrice = BASE_PRICE;
+  if (material === "polycarbonate") totalPrice += PRODUCT_PRICES.material.polycarbonate;
+  if (finish === "textured") totalPrice += PRODUCT_PRICES.finish.textured;
 
-    const { mutate: createPaymentSession } = useMutation({
-      mutationKey: ["get-checkout-session"],
-      mutationFn: createCheckoutSession,
-      onSuccess: ({ url }) => {
-        if (url) router.push(url);
-        else throw new Error("Unable to retrieve payment URL");
-      },
-      onError: () => {
-        toast({
-          title: "Something went wrong",
-          description: "There was an error on our end. Please try again",
-          variant: "destructive",
-        });
-      }
-    });
-
-    const handleCheckout = () => {
-      if (user) {
-        createPaymentSession({ configId: id })
-      } else {
-        localStorage.setItem("configurationId", id)
-        setLoginModalOpen(true);
-      }
+  const { mutate: createPaymentSession } = useMutation({
+    mutationKey: ["get-checkout-session"],
+    mutationFn: createCheckoutSession,
+    onSuccess: ({ url }) => {
+      if (url) router.push(url);
+      else throw new Error("Unable to retrieve payment URL");
+    },
+    onError: () => {
+      toast({
+        title: "Something went wrong",
+        description: "There was an error on our end. Please try again",
+        variant: "destructive",
+      });
     }
+  });
 
-    const { } = useMutation({
-      mutationKey: ["get-checkout-session"]
-    });
+  const handleCheckout = () => {
+    if (user) {
+      createPaymentSession({ configId: id })
+    } else {
+      localStorage.setItem("configurationId", id)
+      setLoginModalOpen(true);
+    }
+  }
 
-    return (
-      <>
-        <div
-          aria-hidden="true"
-          className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center"
-        >
-          <Confetti
-            active={showConfetti}
-            config={{ elementCount: 200, spread: 90, }} />
+  const { } = useMutation({
+    mutationKey: ["get-checkout-session"]
+  });
+
+  return (
+    <>
+      <div
+        aria-hidden="true"
+        className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center"
+      >
+        <Confetti
+          active={showConfetti}
+          config={{ elementCount: 200, spread: 90, }} />
+      </div>
+
+      <LoginModal isOpen={isLoginModalOpen} setIsOpen={setLoginModalOpen} />
+
+      <div className='mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
+        <div className='sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2'>
+          <Phone imgSrc={croppedImgUrl!} className={cn(`bg-${tw}`)} />
         </div>
-
-        <LoginModal isOpen={isLoginModalOpen} setIsOpen={setLoginModalOpen} />
-
-        <div className='mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
-          <div className='sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2'>
-            <Phone imgSrc={croppedImgUrl!} className={cn(`bg-${tw}`)} />
+        <div className='mt-6 sm:col-span-9 sm:mt-0 md:row-end-1'>
+          <h3 className='text-3xl font-bold tracking-tight text-gray-900'>Your {modelLabel} Case</h3>
+          <div className='mt-3 flex items-center gap-1.5 text-base'>
+            <Check className='h-4 w-4 text-green-500' />
+            In stock and ready to ship
           </div>
-          <div className='mt-6 sm:col-span-9 sm:mt-0 md:row-end-1'>
-            <h3 className='text-3xl font-bold tracking-tight text-gray-900'>Your {modelLabel} Case</h3>
-            <div className='mt-3 flex items-center gap-1.5 text-base'>
-              <Check className='h-4 w-4 text-green-500' />
-              In stock and ready to ship
+        </div>
+        <div className='sm:col-span-12 md:col-span-9 text-base'>
+          <div className='grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10'>
+            <div>
+              <p className='font-medium text-zinc-950'> Highlights</p>
+              <ol className='mt-3 text-zinc-700 list-disc list-inside'>
+                <li>Wireless charging compatible</li>
+                <li>TPU shock absorption</li>
+                <li>Packaging made from recycled material</li>
+                <li>5 year print warranty</li>
+              </ol>
+            </div>
+            <div>
+              <p className='font-medium text-zinc-950'>
+                materials
+              </p>
+              <ol className='mt-3 text-zinc-700 list-disc list-inside'>
+                <li>High-quality, durable material</li>
+                <li>Scratch and fingerprint resistant coating</li>
+              </ol>
             </div>
           </div>
-          <div className='sm:col-span-12 md:col-span-9 text-base'>
-            <div className='grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10'>
-              <div>
-                <p className='font-medium text-zinc-950'> Highlights</p>
-                <ol className='mt-3 text-zinc-700 list-disc list-inside'>
-                  <li>Wireless charging compatible</li>
-                  <li>TPU shock absorption</li>
-                  <li>Packaging made from recycled material</li>
-                  <li>5 year print warranty</li>
-                </ol>
-              </div>
-              <div>
-                <p className='font-medium text-zinc-950'>
-                  materials
-                </p>
-                <ol className='mt-3 text-zinc-700 list-disc list-inside'>
-                  <li>High-quality, durable material</li>
-                  <li>Scratch and fingerprint resistant coating</li>
-                </ol>
-              </div>
-            </div>
 
-            <div className='mt-8'>
-              <div className='bg-gray-50 p-6 sm:rounded-lg sm:p-8'>
-                <div className='flow-root text-sm'>
+          <div className='mt-8'>
+            <div className='bg-gray-50 p-6 sm:rounded-lg sm:p-8'>
+              <div className='flow-root text-sm'>
+                <div className='flex items-center justify-between py-1 mt-2'>
+                  <p className='text-gray-600'>Base price</p>
+                  <p className='font-medium text-gray-900'>{formatPrice(BASE_PRICE / 100)}</p>
+                </div>
+
+                {finish === "textured" && (
                   <div className='flex items-center justify-between py-1 mt-2'>
-                    <p className='text-gray-600'>Base price</p>
-                    <p className='font-medium text-gray-900'>{formatPrice(BASE_PRICE / 100)}</p>
+                    <p className='text-gray-600'>Textured finish</p>
+                    <p className='font-medium text-gray-900'>{formatPrice(PRODUCT_PRICES.finish.textured / 100)}</p>
                   </div>
+                )}
 
-                  {finish === "textured" && (
-                    <div className='flex items-center justify-between py-1 mt-2'>
-                      <p className='text-gray-600'>Textured finish</p>
-                      <p className='font-medium text-gray-900'>{formatPrice(PRODUCT_PRICES.finish.textured / 100)}</p>
-                    </div>
-                  )}
-
-                  {material === "polycarbonate" && (
-                    <div className='flex items-center justify-between py-1 mt-2'>
-                      <p className='text-gray-600'>soft polycarbonate material</p>
-                      <p className='font-medium text-gray-900'>{formatPrice(PRODUCT_PRICES.material.polycarbonate / 100)}</p>
-                    </div>
-                  )}
-
-                  <div className='mr-2 h-px bg-gray-200' />
-
-                  <div className='flex items-center justify-between py-2'>
-                    <p className='font-semibold text-gray-900'>
-                      Order total
-                    </p>
-                    <p className='font-semibold text-gray-900'>
-                      {formatPrice(totalPrice / 100)}
-                    </p>
+                {material === "polycarbonate" && (
+                  <div className='flex items-center justify-between py-1 mt-2'>
+                    <p className='text-gray-600'>soft polycarbonate material</p>
+                    <p className='font-medium text-gray-900'>{formatPrice(PRODUCT_PRICES.material.polycarbonate / 100)}</p>
                   </div>
+                )}
+
+                <div className='mr-2 h-px bg-gray-200' />
+
+                <div className='flex items-center justify-between py-2'>
+                  <p className='font-semibold text-gray-900'>
+                    Order total
+                  </p>
+                  <p className='font-semibold text-gray-900'>
+                    {formatPrice(totalPrice / 100)}
+                  </p>
                 </div>
               </div>
+            </div>
 
-              <div className='mt-8 flex justify-end pb-12'>
-                <Button
-                  onClick={handleCheckout}
-                  isLoading={false}
-                  loadingText='loading'
-                  disabled={false}
-                  className='px-4 sm:px-6 lg:px-8'>
-                  check out
-                  <ArrowRight className='h-4 w-4 ml-1.5' />
-                </Button>
-              </div>
+            <div className='mt-8 flex justify-end pb-12'>
+              <Button
+                onClick={handleCheckout}
+                isLoading={false}
+                loadingText='loading'
+                disabled={false}
+                className='px-4 sm:px-6 lg:px-8'>
+                check out
+                <ArrowRight className='h-4 w-4 ml-1.5' />
+              </Button>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 export default DesignPreview;
