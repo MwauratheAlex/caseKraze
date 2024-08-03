92ccc7c5e541447ae155505906f0bb522ef752bc
Mbugua <mwaurathealex@gmail.com>
2024-08-03 14:37:19 +0300

    refinements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/snake-1.png" alt="casecobra logo" className="w-full" />
+                <img src="/snake-1.png" alt="casekraze logo" className="w-full" />
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                 Your Image on a <span className="bg-green-600 px-2 text-white"> Custom </span> Phone Case
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
                 Capture your favourite memories with your phone, <span className="font-semibold">one-of-one</span> phone case.
-                CaseCobra allows you to protect your memories, not just your phone case.
+                CaseKraze allows you to protect your memories, not just your phone case.
               </p>
               <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
                 <div className="space-y-2">
